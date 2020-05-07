9337671a0f556479233b96354329ddc66a37ade0
gschreiner <geomarschreiner@gmail.com>
2020-05-07 19:00:33 -0300

    Aula 07/05

--- src/edu/unoesc/controller/MicroondasController.java
+++ src/edu/unoesc/controller/MicroondasController.java
@@ -23,6 +23,9 @@ public class MicroondasController {
 
 	@Autowired
 	private MicroondasDAO microDao;
+	
+	@Autowired
+	private PessoaDAO pessoaDao;
 
 	@RequestMapping(value = "/microondas", method = RequestMethod.GET)
 	public String microList(Model m) {
@@ -31,6 +34,7 @@ public class MicroondasController {
 
 		m.addAttribute("listMicro", micros);
 		m.addAttribute("micro", new Microondas());
+		m.addAttribute("pessoasList", pessoaDao.getPessoas());
 
 		return "Microondas";
 	}
@@ -44,13 +48,33 @@ public class MicroondasController {
 	}
 	
 	@RequestMapping(value = "/microDelete/{id}")
-	public String pessoa(@PathVariable int id, Model model) {
+	public String microDelete(@PathVariable int id, Model model) {
 
 		if (this.microDao.deleteMicroondas(id))
 			System.out.print("/t -> Deu boa/n");
 		else
 			System.out.print("Deu caca");
 		
+		return "redirect:/microondas";
+	}
+	
+	@RequestMapping(value = "/microDetail/{id}")
+	public String microDetail(@PathVariable int id, Model model) {
+
+		Microondas m = this.microDao.getMicroondasById(id);
+
+		model.addAttribute("micro", m);
+		model.addAttribute("pessoasList", pessoaDao.getPessoas());
+
+		return "MicroondasDetail";
+
+	}
+	
+	@RequestMapping(value = "/microUpdate", method = RequestMethod.POST)
+	public String microEdit(@ModelAttribute("micro") Microondas micro) {
+
+		this.microDao.updateMicroondas(micro);
+
 		return "redirect:/microondas";
 	}
 }
